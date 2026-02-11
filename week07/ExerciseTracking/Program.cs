db48a9c138b0ce4c89e0c25858d8f536bc4ff565
Joseph <epelinggon@byupathway.edu>
2026-02-11 15:18:14 +0800

    week 7 (TRacking Progmram)

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        Running run = new Running("03 Nov 2022", 30, 3.0);
+        activities.Add(run);
+
+        Cycling cycle = new Cycling("03 Nov 2022", 45, 15.0);
+        activities.Add(cycle);
+
+        Swimming swim = new Swimming("03 Nov 2022", 20, 25);
+        activities.Add(swim);
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
